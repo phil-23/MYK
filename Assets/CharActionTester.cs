396c071b592549e8e98f0882cf136354a9aad199
Phil Aitel <philaitel@gmail.com>
2017-07-01 23:23:13 -0700

    Basic setup for testing character package.

---
+++ Assets/CharActionTester.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MYK
+{
+    public class CharActionTester : MonoBehaviour
+    {
+        [System.Serializable]
+        public class AnimAction
+        {
+            public string name;
+            public bool toggle = false;
+            public float duration = 5f;            
+        }
+
+        [SerializeField] private List<AnimAction> actions;
+        [SerializeField] private float timeBetweenActions = 1f;
+        [SerializeField] private Text statusField;
+
+
+        private Animator _anim;
+        private string _lastToggle;
+        
+        private void Start ()
+        {
+            _anim = GetComponent<Animator>();
+            PlayAction();
+        }
+
+        private void PlayAction()
+        {
+            AnimAction act = actions[Random.Range(0, actions.Count)];
+            statusField.text = act.name;
+            if (act.toggle)
+            {
+                _anim.SetBool(act.name, true);
+                _lastToggle = act.name;
+            }
+            else
+            {
+                _anim.SetTrigger(act.name);
+            }
+
+            Invoke("FinishAction", act.duration);
+        }
+
+        private void FinishAction()
+        {
+            if(_lastToggle != null)
+            {
+                _anim.SetBool(_lastToggle, false);
+            }
+
+            Invoke("PlayAction", timeBetweenActions);
+        }
+	}
+}
